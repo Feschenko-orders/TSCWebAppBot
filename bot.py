672e5edf08ebb084f722f84c784649530034d81f
Kirill Feschenko <feschenko@protonmail.com>
2024-06-24 18:55:42 +0300

    Init.

---
+++ bot.py
@@ -0,0 +1,35 @@
+import asyncio
+import logging
+import sys
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram import types
+from config import Config
+
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: types.Message) -> None:
+    await message.answer(
+        f"Hello, <b>{message.from_user.full_name}</b>!",
+        menu_button=types.MenuButtonWebApp(
+            text="Open Menu", web_app=types.WebAppInfo(url=f"{Config.BOT.BASE_URL}/demo")
+        ),
+    )
+
+
+async def main() -> None:
+    bot = Bot(
+        token=Config.BOT.TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
